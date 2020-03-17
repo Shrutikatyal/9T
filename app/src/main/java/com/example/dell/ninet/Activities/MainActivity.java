3933cba456b0083e525634e19f68572255e76c62
avishi <goyalavishi@gmail.com>
2020-03-17 05:05:32 -0400

    nav bar

--- app/src/main/java/com/example/dell/ninet/Activities/MainActivity.java
+++ app/src/main/java/com/example/dell/ninet/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.dell.ninet;
+package com.example.dell.ninet.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.dell.ninet.R;
 
 public class MainActivity extends AppCompatActivity {
 
